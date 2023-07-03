9d45cadf36619a0c22320ee07cb9c4e0fc0dff98
Jeanluca Moreno <jeanlucam12@gmail.com>
2023-07-02 21:00:05 -0300

    chore: adding project's color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './App.{js,jsx,ts,tsx}',
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         anybodyRegular: 'Anybody',
         anybodyMedium: 'Anybody_Medium',
         anybodyBold: 'Anybody_Bold',
-      }
+      },
+      colors: {
+        brandRed: '#f50000',
+        brandBlack: '#263238',
+        brandWhite: '#f2f2f2',
+      },
     },
   },
   plugins: [],
