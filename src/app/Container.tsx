7b268a0f7239e14ed3937f47d6ab88d799fe4c26
Jeanluca Moreno <jeanlucam12@gmail.com>
2023-07-04 23:00:15 -0300

    fix: adjusting padding in the main container

--- src/app/Container.tsx
+++ src/app/Container.tsx
@@ -2,5 +2,5 @@ import { SafeAreaView, Text } from 'react-native';
 import React from 'react';
 
 export default function Container({ children }) {
-  return <SafeAreaView className="p-6">{children}</SafeAreaView>;
+  return <SafeAreaView className="px-6">{children}</SafeAreaView>;
 }
