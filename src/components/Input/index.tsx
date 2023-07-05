dfe7740e248941f9f4f3a76356216d249b5cfea0
Jeanluca Moreno <jeanlucam12@gmail.com>
2023-07-04 23:00:01 -0300

    feat: adding onFocus stylization

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,9 @@
 import Eye from 'phosphor-react-native/src/icons/Eye';
 import EyeClosed from 'phosphor-react-native/src/icons/EyeClosed';
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import { TextInput, TouchableOpacity, View } from 'react-native';
 import { InputPropTypes } from './types';
+import globalTheme from '../../styles/globalTheme';
 
 export function Input({
   placeholder = 'Texto',
@@ -14,25 +15,37 @@ export function Input({
 }: InputPropTypes) {
   const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
   const [inputValue, setInputValue] = useState<string | undefined>(value);
-
-  console.log(inputValue?.length)
-
+  const [isFocused, setIsFocused] = useState<boolean>(false);
 
   function handlePasswordIsVisible() {
     setIsPasswordVisible(!isPasswordVisible);
   }
+
+  function handleIsFocused() {
+    setIsFocused(true);
+  }
+  function handleIsBlurred() {
+    setIsFocused(false);
+  }
+
   return (
     <View
-      className={`p-2 flex-row items-center border-2 border-black-brandBlack text-black-brandBlack rounded-lg  `}
+      className={`flex-row items-center border-2 ${
+        isFocused ? 'border-outer-space-600' : 'border-outer-space-950 '
+      }  text-outer-space-950 rounded-lg`}
     >
       <TextInput
-        className={`w-[90%] font-anybodyRegular text-base placeholder:${
-          inputValue?.length > 0 ? 'no-underline' : 'underline'
+        className={`${
+          type === 'password' ? 'w-[90%]' : 'w-full'
+        } p-2 font-anybodyRegular text-base ${
+          inputValue?.length > 0
+            ? 'placeholder:no-underline'
+            : 'placeholder:underline'
         }`}
         defaultValue={inputValue}
         placeholder={placeholder}
-        placeholderTextColor="#263238"
-        selectionColor="#263238"
+        placeholderTextColor={globalTheme.colors['outer-space'][950]}
+        selectionColor={globalTheme.colors['outer-space'][950]}
         secureTextEntry={isPasswordVisible}
         keyboardType={keyboardType}
         onChangeText={(insertedText) => setInputValue(insertedText)}
@@ -43,11 +56,12 @@ export function Input({
           right: type !== 'password' ? 8 : undefined,
         }}
         ref={inputRef}
+        onFocus={handleIsFocused}
+        onBlur={handleIsBlurred}
         {...props}
       />
       {type === 'password' ? (
         <TouchableOpacity
-          className=""
           onPress={handlePasswordIsVisible}
           hitSlop={{ bottom: 8, top: 8, left: 8, right: 8 }}
         >
