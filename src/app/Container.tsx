67183be8962307fba092cba9f99e11274ab6bd88
Jeanluca Moreno <jeanlucam12@gmail.com>
2023-07-07 17:47:15 -0300

    chore: adding validation of keyboard to container

--- src/app/Container.tsx
+++ src/app/Container.tsx
@@ -1,6 +1,19 @@
-import { SafeAreaView, Text } from 'react-native';
-import React from 'react';
+import { SafeAreaView, ScrollView } from 'react-native';
+import React, { useMemo } from 'react';
+import { useKeyboard } from '../hooks/useKeyboard';
 
 export default function Container({ children }) {
-  return <SafeAreaView className="px-6">{children}</SafeAreaView>;
+  const { isKeyboardOpen } = useKeyboard();
+
+  const paddingIfKeyboard = useMemo(() => {
+    return isKeyboardOpen ? 'px-6' : 'p-6';
+  }, [isKeyboardOpen]);
+
+  return (
+    <SafeAreaView className={`${paddingIfKeyboard} flex-1`}>
+      <ScrollView bounces={false} showsVerticalScrollIndicator={false}>
+        {children}
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
